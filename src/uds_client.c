32489710f88b6ed4f03a85cd88e2b64a38e48176
shinokun <hypatia.tile02021@gmail.com>
2026-01-27 09:26:30 +0900

    Modified unix domain socket examples
    Added error handling function with descriptive messages for better debugging.
    Applied it to connection generation such as connect, bind, and listen.

--- src/uds_client.c
+++ src/uds_client.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+
+void die(const char *msg) {
+  perror(msg);
+  exit(1);
+}
 
 int main(void) {
   int s = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -10,10 +17,12 @@ int main(void) {
   addr.sun_family = AF_UNIX;
   strcpy(addr.sun_path, "./test.sock");
 
-  connect(s, (struct sockaddr *)&addr, sizeof(addr));
+  if (connect(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    die("connect");
 
   write(s, "A", 1);
   write(s, "B", 1);
 
-  close(s);
+  if (close(s) < 0)
+    die("close");
 }
--- src/uds_server.c
+++ src/uds_server.c
@@ -5,6 +5,11 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+void die(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
 int main(void) {
   const char *path = "./test.sock";
   unlink(path);
@@ -14,10 +19,17 @@ int main(void) {
   addr.sun_family = AF_UNIX;
   strcpy(addr.sun_path, path);
 
-  bind(s, (struct sockaddr *)&addr, sizeof(addr));
-  listen(s, 1);
+  if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    die("bind");
 
-  int c = accept(s, NULL, NULL);
+  if (listen(s, 1) < 0)
+    die("listen");
+
+  int c = accept(s, NULL, NULL); // Accept returns the file descriptor for the
+                                 // connection if successful
+
+  if (c < 0)
+    die("accept");
 
   char buf[16];
   ssize_t n = read(c, buf, sizeof(buf));
